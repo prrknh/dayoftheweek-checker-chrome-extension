27dd8c6651830a188733c4da3f630f82241ca003
prrknh <prrknh@gmail.com>
2021-03-20 15:18:13 +0900

    show alert when include invalid

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import getInvalidDayOfTheWeekList from "./CheckDayOfTheWeek";
 
+const mark = "markedByCheckedOfTheWeekChromeExtension";
+
 function onChanged(e: Event) {
-  getInvalidDayOfTheWeekList((e.target as HTMLTextAreaElement).value).forEach(
-    (s) => {
-      console.log(
-        `${s.targetDateStr}は${s.invalidDayOfTheWeek}ではなく${s.validDayOfTheWeek}です`
-      );
-    }
+  const list = getInvalidDayOfTheWeekList(
+    (e.target as HTMLTextAreaElement).value
   );
+  list.forEach((s) => {
+    console.log(
+      `${s.targetDateStr}は${s.invalidDayOfTheWeek}ではなく${s.validDayOfTheWeek}です`
+    );
+  });
+  if (list.length > 0) {
+    (e.target as HTMLElement).setAttribute("id", mark);
+  }
 }
 
 export function listen() {
@@ -16,4 +22,16 @@ export function listen() {
   });
 }
 
+function setStyle() {
+  const styleElement = document.createElement("style");
+  document.head.appendChild(styleElement);
+  if (styleElement.sheet) {
+    styleElement.sheet?.insertRule(
+      `textarea#${mark} {border-color:red; border-width: thick;}`,
+      0
+    );
+  }
+}
+
+setStyle();
 listen();
