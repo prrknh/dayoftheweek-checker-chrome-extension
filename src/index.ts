532540ebce86fa9dfafc4070367d9b8a1110521b
prrknh <prrknh@gmail.com>
2021-03-20 12:55:58 +0900

    base setting

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export default function f() {
+  console.log("hello");
+}
+
+f();
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist/')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: [
+      '.ts', '.js',
+    ],
+  },
+  target: ["es5"],
+};
+
