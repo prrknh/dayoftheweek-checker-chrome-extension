b1261f1eb9be7b7f2f58ba3e4237eea8803b44e4
prrknh <prrknh@gmail.com>
2021-03-22 00:29:55 +0900

    show green result to notify actually checked

--- src/contents/actions/listenInput.ts
+++ src/contents/actions/listenInput.ts
@@ -15,14 +15,14 @@ function onFocusOut(e: Event) {
   const checker = new DayOfTheWeekChecker(
     (e.target as HTMLTextAreaElement).value
   );
-  if (checker.hasInvalid()) {
-    showCheckResult(checker);
-    (e.target as HTMLElement).classList.add(mark);
-  }
+  showCheckResult(checker);
+  (e.target as HTMLElement).classList.add(
+    `${checker.hasInvalid() ? "invalid" : "valid"}${mark}`
+  );
 }
 
 function onFocus(e: Event) {
-  (e.target as HTMLElement).classList.remove(mark);
+  (e.target as HTMLElement).classList.remove(`invalid${mark}`, `valid${mark}`);
 }
 
 function setStyle() {
@@ -30,7 +30,11 @@ function setStyle() {
   document.head.appendChild(styleElement);
   if (styleElement.sheet) {
     styleElement.sheet?.insertRule(
-      `textarea.${mark} {border-color:red; border-width: thick;}`,
+      `textarea.invalid${mark} {border-color:red; border-width: thick;}`,
+      0
+    );
+    styleElement.sheet?.insertRule(
+      `textarea.valid${mark} {border-color:lime; border-width: thick;}`,
       0
     );
   }
